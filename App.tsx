c0f6c1d43b02adc1cdc06db3bb029883ee012bd6
BubyrYehorDev <bubyryehordev@gmail.com>
2022-06-09 14:42:09 +0300

    setup redux and redux saga

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {Provider} from 'react-redux';
+import store from './src/store';
 import {TabNav} from './src/navigation/TabNav/Tab';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <TabNav />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <TabNav />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
